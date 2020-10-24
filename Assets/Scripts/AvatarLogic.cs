8a07d5e9e3e1023ebf5e107ceb562b5273384611
Jazmin Romero <ra190899@alumno.udb.edu.sv>
2020-10-24 11:41:16 -0600

    trigger&collisions

---
+++ Assets/Scripts/AvatarLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarLogic : MonoBehaviour
+{
+    public float speedMovement = 5.0f;
+    public float speedRotation = 200.0f;
+    private Animator anim;
+    public float x, y;
+    public float vida;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        vida = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, x * Time.deltaTime * speedRotation, 0);
+        transform.Translate(0,0, y * Time.deltaTime * speedMovement);
+
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    
+    }
+
+    void OnTriggerEnter(Collider colisionador){
+
+        if (colisionador.tag=="Item")
+        {
+            vida += 5.0f;
+            Destroy (colisionador.gameObject);
+        }else if(colisionador.tag=="Itemvidamenos"){
+            vida -= 5.0f;
+            Destroy (colisionador.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/colorSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+
+     void OnCollisionExit(Collision collision){
+
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
