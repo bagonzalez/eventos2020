a20f36061d756494acddd0e18fc37509f67f57f4
Rodrigo Baires <rodrigobaires13@gmail.com>
2020-10-24 19:44:51 -0600

    Trigger

---
+++ Assets/Destruccion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruccion : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("DestrucTrig"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
